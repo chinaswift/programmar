bcd512e0e9d91166d097e1bd9d2b45a8cca20404
Joseph Smith <hi@joesmith.me>
2015-02-19 10:34:22 +0000

    A - Fixed pasting content

--- public/js/vendor/angular-wysiwyg.js
+++ public/js/vendor/angular-wysiwyg.js
@@ -97,7 +97,39 @@ angular.module('wysiwyg.module', ['colorpicker.module'])
                     if (html == '<br>') {
                         html = '';
                     }
+
                     ngModelController.$setViewValue(html);
+                });
+
+                textarea.on('paste', function(e) {
+                    e.preventDefault();
+
+                    var text = (e.originalEvent || e).clipboardData.getData('text/html') || prompt('Paste something..');
+                    var $result = $('<div></div>').append($(text));
+
+                    $(this).html($result.html());
+
+                    // replace all styles except bold and italic
+                    $.each($(this).find("*"), function(idx, val) {
+
+                        var $item = $(val);
+                        if ($item.length > 0){
+                           var saveStyle = {
+                                'font-weight': $item.css('font-weight'),
+                                'font-style': $item.css('font-style')
+                            };
+                            $item.removeAttr('style')
+                                 .removeClass()
+                                 .css(saveStyle);
+                        }
+                    });
+
+                    // remove unnecesary tags (if paste from word)
+                    $(this).children('style').remove();
+                    $(this).children('pre').remove();
+                    $(this).children('code').remove();
+                    $(this).children('meta').remove()
+                    $(this).children('link').remove();
                 });
 
 
