3db06f620f1e2b36b1c1445a945a3dcf48ec8552
Joseph Smith <hi@joesmith.me>
2015-02-21 04:04:23 +0000

    A - tried updating API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends HttpKernel {
 		'Illuminate\Session\Middleware\StartSession',
 		'Illuminate\View\Middleware\ShareErrorsFromSession',
 		'App\Http\Middleware\VerifyCsrfToken',
-		'App\Http\Middleware\APIResponseHeaderMiddleware'
+		'App\Http\Middleware\APIResponseHeaderMiddleware',
 	];
 
 	/**
@@ -28,6 +28,7 @@ class Kernel extends HttpKernel {
 		'auth' => 'App\Http\Middleware\Authenticate',
 		'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
 		'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
+		'cors' => 'App\Http\Middleware\APIResponseHeaderMiddleware',
 	];
 
 }
