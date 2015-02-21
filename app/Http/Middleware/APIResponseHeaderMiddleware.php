6ece5750aec35b98fd462972885befcbc2b6f84c
Joseph Smith <hi@joesmith.me>
2015-02-21 12:52:23 +0000

    A - testing update

--- app/Http/Middleware/APIResponseHeaderMiddleware.php
+++ app/Http/Middleware/APIResponseHeaderMiddleware.php
@@ -3,6 +3,7 @@
 use Closure;
 use Illuminate\Contracts\Routing\Middleware;
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Response;
 
 class APIResponseHeaderMiddleware implements Middleware {
 
@@ -14,10 +15,10 @@ class APIResponseHeaderMiddleware implements Middleware {
   * @return mixed
   */
   public function handle($request, Closure $next)
-  {
-    return $next($request)->header('Access-Control-Allow-Origin' , '*')
-      ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-      ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
-  }
-
+ {
+  $content = $next($request);
+  return ( new Response($content) )->header('Access-Control-Allow-Origin' , '*')
+                                   ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                                   ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin');
+ }
 }
