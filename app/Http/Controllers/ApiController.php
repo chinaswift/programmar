6325cb9104b8910b6df7de4abedc19361fb98a10
Joseph Smith <hi@joesmith.me>
2015-02-21 10:54:33 +0000

    A - made change

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -21,7 +21,7 @@ class ApiController extends Controller {
 		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 		$data = curl_exec($ch);
 		curl_close($ch);
-		return json_decode($data, true);
+		return $data;
 	}
 
 	//Collect the users followers
