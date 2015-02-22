dc279114d2754ea8b14f7eeb4fe6b2c0bb0b88fa
Joseph Smith <hi@joesmith.me>
2015-02-22 00:03:58 +0000

    A - update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,7 +33,7 @@ class HomeController extends Controller {
 
 			if($followersCount > 0) {
 				foreach($followers as $follower) {
-					$following_user = User::find($follower->followed);
+					$following_user = User::find($follower->followed)->first();
 					dd($following_user);
 					$array = array(
 						'user_id' => $following_user->id,
