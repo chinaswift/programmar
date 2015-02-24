67b3b4799bef45442e4b10a9ea51f4f6ce1cd3c0
Joseph Smith <hi@joesmith.me>
2015-02-24 00:28:35 +0000

    A - updated sideback

--- resources/views/home/includes/sidebar.blade.php
+++ resources/views/home/includes/sidebar.blade.php
@@ -28,15 +28,7 @@ if(!isset($following)) {
 	<a href="/all" class="{{ $all }}">All</a>
 	<a href="/popular" class="{{ $popular }}">Popular</a>
 	@if(Auth::check())
+		<a href="/following" class="{{ $following }}">Following</a>
 		<a href="/drafts" class="{{ $drafts }}">Drafts</a>
-
-		<div class="options">
-			<a href="/following" class="{{ $following }}">Following</a>
-			<div class="clearfix profile-img-cont">
-				@foreach($followers as $follower)
-					<a href="/dev/{{ $follower['user_slug'] }}"><img src="{{$follower['user_avatar']}}" class="img-circle profile-image"></a>
-				@endforeach
-			</div>
-		</div>
 	@endif
 </aside>
