afa06adb4d1595823d54d742ad2dd5b01dac877e
Joseph Smith <joseph.smithy@outlook.com>
2015-09-24 21:04:12 +0100

    Updated article part view

--- resources/views/_partials/article-part.blade.php
+++ resources/views/_partials/article-part.blade.php
@@ -3,16 +3,17 @@
 		<div class="col-md-1 col-xs-2 upvote-cont">
 				@include('_partials.article-upvote')
 		</div>
-		<div class="col-md-9 col-xs-8">
+		<div class="col-md-9 col-xs-10">
 			<div class="article--part_title">
-				<a href="/user/@{{ article.owner_user }}"><img ng-src="@{{ article.owner_img }}" class="profile--image image--small"></a>
+				<a href="/user/@{{ article.owner_user }}" class="hidden-sm-down"><img ng-src="@{{ article.owner_img }}" class="profile--image image--small"></a>
 				<a href="/articles/@{{ article.id }}"><h3>@{{ article.name }}</h3></a>
+				<span class="article--part_time hidden-md-up">@{{ article.time_ago }}</span>
 			</div>
 			<div class="article--part_content">
 				<p>@{{ article.contentHTML | cut:true:160:' ...' }}</p>
 			</div>
 		</div>
-		<div class="col-xs-2">
+		<div class="col-xs-2 hidden-sm-down">
 			<span class="article--part_time">@{{ article.time_ago }}</span>
 		</div>
 	</div>
