48eb7d75903844d6c737c141a780ba2e9dbb09a6
Joseph Smith <joseph.smithy@outlook.com>
2015-09-25 06:01:16 +0100

    Added footer responsive classes

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="main-footer" ng-cloak ng-show="!articlesLoading">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-4 col-xs-6">
 				&copy; 2015 Programmar
 			</div>
 
-			<div class="col-md-4 text-center">
+			<div class="col-md-4 text-center hidden-sm-down">
 				<a href="http://api.programmar.io" target="_blank">Documentation</a>
 				<a href="https://github.com/Layerful/programmar" target="_blank">GitHub</a>
 			</div>
 
-			<div class="col-md-4 text-right">
+			<div class="col-md-4 col-xs-6 text-right">
 				<a href="http://twitter.com/_programmar" class="twitter" target="_blank">@_programmar</a>
 				<a href="https://layerful.com/" target="_blank">Layerful</a>
 			</div>
